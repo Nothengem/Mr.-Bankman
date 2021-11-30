1f299ffe5d60040ebcab10706dfe2011cc156999
Alexey Kochanov <nothengem43656@gmail.com>
2021-11-30 21:04:03 +0300

    + охранник и Бенеттон

--- DataBase/CharacterPortraitDataBase.gd
+++ DataBase/CharacterPortraitDataBase.gd
@@ -26,8 +26,7 @@ const DATA = {
 	"Crowley Shepard" : [ "Head8", "Neck1", "Shirt11", "Eyebrows12", "Eyes2", "Forehead2", "Ears1", "Jowls0", "Glasses0", "Mouth2", "Hair6", "Nose10"],
 	"Sanna Acol Nitt" : [ "Head8", "Neck1", "Shirt13", "Eyebrows8", "Eyes15", "Forehead0", "Ears1", "Jowls2", "Glasses0", "Mouth8", "Hair8", "Nose4"],
 	"Darls Coy" : [ "Head5", "Neck1", "Shirt7", "Eyebrows6", "Eyes1", "Forehead0", "Ears8", "Jowls0", "Glasses0", "Mouth6", "Hair1", "Nose8"],
-	
-	"Mafiosi" : [ "Head4", "Neck1", "Shirt4", "Eyebrows4", "Eyes4", "Forehead0", "Ears2", "Jowls1", "Glasses0", "Mouth2", "Hair3", "Nose2"],
+	"Bantonio Anderas" : [ "Head9", "Neck1", "Shirt7", "Eyebrows1", "Eyes4", "Forehead1", "Ears5", "Jowls0", "Glasses0", "Mouth1", "Hair6", "Nose15"],
 	
 	"GrishaGrachev" : [ "Head5", "Neck1", "Shirt3", "Eyebrows3", "Eyes5", "Forehead1", "Ears3", "Jowls0", "Glasses1", "Mouth1", "Hair4", "Nose2"],
 	
