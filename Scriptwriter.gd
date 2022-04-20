c6e75df3aa56d478f0bb72d316d2978478bcc759
Alexey Kochanov <nothengem43656@gmail.com>
2022-04-20 19:25:10 +0300

    вторые пробы

--- Scriptwriter.gd
+++ Scriptwriter.gd
@@ -196,20 +196,42 @@ func _ready():
 	victory_count = 0
 	position_on_level = 0
 	
-	characterPortraitArtBase.Head[0] = load( str ("res://Resources/GFX/CharacterCotaint/Head/Head1.png"))
-	print(characterPortraitArtBase.Head[0])
+	var files = list_files_in_directory("res://Resources/GFX/CharacterCotaint/Head/")
+	list_files_in_directory(files)
+	print(files)
+	
+#	characterPortraitArtBase.Head[0] = load( str ("res://Resources/GFX/CharacterCotaint/Head/Head1.png"))
+#	print(characterPortraitArtBase.Head[0])
 	
 #	if CurrentLevel >= 3:
 #		NameRule = true
 #		PhotoRule = true
 
-func artsLoader(folder):
-	var wayToPicture = "res://Resources/GFX/CharacterCotaint/"
-	var variableLoader = ""
-	wayToPicture = wayToPicture + folder
-	while variableLoader != null:
-		pass
+#func artsLoader(folder):
+#	var wayToPicture = "res://Resources/GFX/CharacterCotaint/"
+#	var variableLoader = ""
+#	wayToPicture = wayToPicture + folder
+#	while variableLoader != null:
+#		pass
 	
+	
+	
+func list_files_in_directory(path):
+	var files = []
+	var dir = Directory.new()
+	dir.open(path)
+	dir.list_dir_begin()
+
+	while true:
+		var file = dir.get_next()
+		if file == "":
+			break
+		elif not file.begins_with("."):
+			files.append(file)
+
+	dir.list_dir_end()
+
+	return files
 	
 
 func level_massive_generator():
